fd5dbfa8a3a138e09bdc1521c6225a67970d8e92
unknown <W9005298@adc.com>
2020-09-23 15:11:36 +0800

    安装依赖

--- optionchain/src/App.vue
+++ optionchain/src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="testOption">点我测试</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      a: {
+        b: {
+          c: "1",
+        },
+      },
+    };
+  },
+  methods: {
+    testOption() {
+      alert(this.a?.b?.c);
+    },
+  },
+};
 </script>
 
 <style>
